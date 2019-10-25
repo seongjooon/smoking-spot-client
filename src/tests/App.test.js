206c39bedbe0d737f6d65b81f34bb5af003c7bde
MrKamake <seongjooon@gmail.com>
2019-10-25 16:19:33 +0900

    Create App router test file

---
+++ src/tests/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Route, Redirect } from 'react-router-dom';
+import App from '../components/App';
+import Map from '../components/Map';
+
+describe('<App />', () => {
+  it('routes changing url', () => {
+    const wrapper = shallow(
+      <App initialEntries={['/spots/map']}>
+        <Route exact path='/spots/map' render={() => <Map />} />
+      </App>
+    );
+
+    // expect(wrapper.find(Map)).toHaveLength(1);
+  });
+});
